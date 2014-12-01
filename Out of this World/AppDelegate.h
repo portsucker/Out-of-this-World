b38fcf36a01528324aa634c73f07b1107fbf4604
portsucker <kush@Kushs-MacBook-Pro.local>
2014-12-01 13:27:54 +0800

    Initial Commit

---
+++ Out of this World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out of this World
+//
+//  Created by Kush Shah on 1/12/2014.
+//  Copyright (c) 2014 Kush Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
